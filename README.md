7fc8fc6979f8fb843c27566f7c69937df9f10fe0
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2018-08-27 16:07:17 +0500

    return question objects as plain text

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Rust-QuizzicalAPI
+
+- [ ] return categories as string
+- [ ] return questions as string
+- [ ] returns responses as json
+- [ ] parse page size and offset from request
+- [ ] return paginated questions
+- [ ] handle errors
+- [ ] cache responses
+
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ extern crate mopa;//makes downcasting from T -> Object easier.
 extern crate bson;
 extern crate mongodb;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
 use std::sync::Arc;
 use std::collections::HashMap;
-use std::ops::Deref;
 
 use hyper::{Body, Request, Response, Server};
 use hyper::rt::Future;
@@ -18,14 +22,13 @@ use hyper::service::service_fn;
 use hyper::{Method, StatusCode};
 
 use mongodb::{Client, ThreadedClient};
-use mongodb::db::{Database, ThreadedDatabase};
+use mongodb::db::{ThreadedDatabase};
 use mongodb::coll::Collection;
+use mongodb::coll::options::FindOptions;
 
 use futures::future;
 
 type BoxedResponse = Box<Future<Item=Response<Body>,Error=hyper::Error> + Send>;
-
-const API_ROOT: &'static str = "/api/v3";
 
 fn app(req: Request<Body>, registry: Arc<ControllerRegistry>) -> BoxedResponse {
     let mut response = Response::new(Body::empty());
@@ -35,8 +38,12 @@ fn app(req: Request<Body>, registry: Arc<ControllerRegistry>) -> BoxedResponse {
             *response.body_mut() = Body::from("Try POSTing data to /echo");
         },
         (&Method::GET, "/api/v3/categories") =>{
-            let controller : &CategoriesController = registry.get("Categories").unwrap();
+            let controller : &QuestionsController = registry.get("Questions").unwrap();
             controller.categories(&req,&mut response)
+        },
+        (&Method::GET, "/api/v3/questions") =>{
+            let controller : &QuestionsController = registry.get("Questions").unwrap();
+            controller.questions(&req,&mut response)
         },
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
@@ -44,6 +51,21 @@ fn app(req: Request<Body>, registry: Arc<ControllerRegistry>) -> BoxedResponse {
     };
 
     Box::new(future::ok(response))
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Choice{
+    title: String,
+    correct: bool
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Question{
+    #[serde(rename = "_id")]
+    id: bson::oid::ObjectId,//not ideal
+    question: String,
+    category: String,
+    choices: Vec<Choice>
 }
 
 struct QuestionsRepository{
@@ -63,19 +85,48 @@ impl QuestionsRepository{
     }
 
     fn categories(&self) -> Result<Vec<String>,String>{
-        return self.coll("questions").distinct("Cat2egory",None,None)
-            .map(|bsons| bsons.iter().map(|bson| bson.as_str().expect("Unexpected non-string category").to_owned() ).collect() )
+        return self
+            .coll("questions")
+            .distinct("category",None,None)
+            .map(|bsons| 
+                bsons
+                .iter()
+                .map(|bson| 
+                    bson
+                    .as_str()
+                    .expect("Unexpected non-string category")
+                    .to_owned() 
+                ).collect()
+            ).map_err(|err| format!("{:?}",err))
+    }
+
+    fn questions(&self,category: &str, page: u64, size: u64)->Result<Vec<Question>,String>{
+        let mut find_options = FindOptions::new();
+        find_options.limit = Some(size as i64);
+        find_options.skip = Some(page.saturating_sub(1).checked_mul(size).unwrap_or(0) as i64);
+
+        let filter = doc!{"category":category.clone()};
+
+        return self
+            .coll("questions")
+            .find(Some(filter),Some(find_options))
+            .map(|c| {
+                c.map(|d|{
+                        bson::from_bson(bson::Bson::Document(d.unwrap()))
+                        .unwrap()
+                }).collect()
+            })
             .map_err(|err| format!("{:?}",err))
     }
 }
 
-struct CategoriesService{
+struct QuestionsService{
     repo : Box<QuestionsRepository>
 }
 
-impl CategoriesService{
-    fn new(repo : QuestionsRepository)->CategoriesService{
-        return CategoriesService{
+impl QuestionsService{
+    fn new(repo : QuestionsRepository)->QuestionsService{
+        return QuestionsService{
             repo : Box::new(repo)
         }
     }
@@ -83,28 +134,37 @@ impl CategoriesService{
     fn categories(&self) -> Result<Vec<String>, String>{
         return self.repo.categories();
     }
+
+    fn questions(&self, category: &str, page: u64, size: u64)->Result<Vec<Question>,String>{
+        return self.repo.questions(category,page,size);
+    }
 }
 
 trait Controller : mopa::Any+Sync+Send {}
 mopafy!(Controller);
 
-struct CategoriesController{
-    service : Box<CategoriesService>
+struct QuestionsController{
+    service : Box<QuestionsService>
 }
 
-impl CategoriesController{
-    fn new(service : CategoriesService)->CategoriesController{
-        return CategoriesController{
+impl QuestionsController{
+    fn new(service : QuestionsService)->QuestionsController{
+        return QuestionsController{
             service : Box::new(service)
         }
     }
 
-    fn categories(&self, request: &Request<Body>, response: &mut Response<Body>)->(){
+    fn categories(&self, _: &Request<Body>, response: &mut Response<Body>)->(){
         *response.body_mut() = Body::from(self.service.categories().unwrap().join(",").to_string());        
+    }
+
+    fn questions(&self, request: &Request<Body>, response: &mut Response<Body>)->(){
+        //*response.body_mut() = Body::from(self.service.questions("Science",1,10).unwrap().join("\n").to_string()));        
+        *response.body_mut() = Body::from(format!("{:?}",self.service.questions("Science",1,10).unwrap()));        
     }
 }
 
-impl Controller for CategoriesController{}
+impl Controller for QuestionsController{}
 
 struct ControllerRegistry{
     register : HashMap<&'static str, Box<Controller + 'static>>
@@ -132,10 +192,10 @@ fn main() {
         .expect("Failed to initialize standalone client.");
 
     let repo = QuestionsRepository::new(client);
-    let service = CategoriesService::new(repo); 
-    let controller = CategoriesController::new(service);
+    let service = QuestionsService::new(repo); 
+    let controller = QuestionsController::new(service);
     let mut registry = ControllerRegistry::new();
-    registry.put("Categories",controller);
+    registry.put("Questions",controller);
 
     let registry_ref = Arc::new(registry);
     //move registry_ref into new_service closure so that new_service closure will own registry.
